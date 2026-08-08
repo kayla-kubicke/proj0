3d1ce6dffc810e917228086dee3b71e6d5da1716
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2026-08-08 13:28:19 -0700

    All Tests Pass

--- src/game2048logic/Model.java
+++ src/game2048logic/Model.java
@@ -193,25 +193,29 @@ public class Model {
         int myValue = currTile.value();
         int targetY = y;
 
-        // while (!targetY == 3) {
-            // // condition 1: tile above is empty, also safe checks null value
-            // if (if board.tile(x, (y+1)) == null) {
-                // targetY = targetY + 1;
-                // continue; (or break?)
-            // }
+         while (targetY != 3) {
+             // condition 1: tile above is empty
+             // also prevents null tile falling into future conditions
+             if (board.tile(x, (targetY + 1)) == null) {
+                 targetY = targetY + 1;
+                 continue;
+             }
 
-            // Tile aboveTile = board.tile(x, (targetY + 1));
-            // // condition 2: the tile above is not equal to current tile
-            // // OR above tile is equal to the current tile but the current tile has already merged
-            // if (currTile != aboveTile || currTile == aboveTile && currTile.merged == true) {
-                // board.move(x, targetY, currTile);
-            // // condition 3: tile has not been merged and tile is equal to above tile
-            // } else if (currTile == aboveTile && currTile.merged == false) {
-                // targetY = targetY + 1;
-                // board.move(x, targetY, currTile);
-        // }
+             Tile aboveTile = board.tile(x, (targetY + 1));
+             // condition 2: the tile above is not equal to current tile
+             // OR above tile is equal to the current tile but the current tile has already merged
+             // * Left in 'extra' condition code for readability.
+             if (myValue != aboveTile.value() || myValue == aboveTile.value() && currTile.wasMerged()) {
+                 break;
+             // condition 3: tile has not been merged and tile is equal to above tile
+             } else if (myValue == aboveTile.value() && !currTile.wasMerged()) {
+                 targetY = targetY + 1;
+                 score = score + (myValue * 2);
+                 break;
+             }
+         }
 
-        // board.move(x, 3, currTile);
+         board.move(x, targetY, currTile);
     }
 
     /** Handles the movements of the tilt in column x of the board
